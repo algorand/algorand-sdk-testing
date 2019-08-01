9c211e8c0d01241f59147ba5007c390c18acd022
mjiang102628 <mjiang102628@gmail.com>
2019-08-01 11:29:24 -0400

    get current commit
    supports getting current commit in travis builds instead of latest commit to master

--- scripts/sdkupdate.sh
+++ scripts/sdkupdate.sh
@@ -1,19 +1,22 @@
 mkdir ~/algorand-sdk-testing
 git clone https://github.com/algorand/algorand-sdk-testing.git ~/algorand-sdk-testing
 cd ~/algorand-sdk-testing
-scripts/setup.sh
 
 case "$1" in 
     --go*)
+        scripts/setup.sh --go
         scripts/test.sh --go --cross
         ;;
     --java*)
+        scripts/setup.sh --java
         scripts/test.sh --java --cross
         ;;
     --js*)
+        scripts/setup.sh --js
         scripts/test.sh --js --cross
         ;;
     --py*)
+        scripts/setup.sh --py
         scripts/test.sh --py --cross
         ;;
 esac
--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,20 +1,45 @@
 #!/usr/bin/env bash
 
 go get github.com/DATA-DOG/godog/cmd/godog
-go get -u github.com/algorand/go-algorand-sdk/...
+if [ ! "$1" in --go ]
+then
+    go get -u github.com/algorand/go-algorand-sdk/...
+fi
 
 pip3 install behave
-pip3 install git+https://github.com/algorand/py-algorand-sdk/#egg=py-algorand-sdk
+if [ "$1" in --py ]
+then
+    pip3 install "git+https://github.com/algorand/py-algorand-sdk@$TRAVIS_COMMIT"
+else
+    pip3 install git+https://github.com/algorand/py-algorand-sdk/
+fi
 
 cd js_cucumber
 npm install
+if [ "$1" in --js ]
+then
+    npm install --save "algorand/js-algorand-sdk#$TRAVIS_COMMIT"
+fi
+
+cd ../java_cucumber
+if [ "$1" in --java ]
+then
+    mvn versions:use-dep-version -Dincludes=com.github.algorand:java-algorand-sdk -DdepVersion=$TRAVIS_COMMIT -DforceVersion=true
+fi
+
+
 
 # get algorand tools; comment this section out if you already have this
 cd ..
 mkdir ~/inst
+# this is the link for linux; change this if on mac or windows
 curl -L https://github.com/algorand/go-algorand-doc/blob/master/downloads/installers/linux_amd64/install_master_linux-amd64.tar.gz?raw=true -o ~/inst/installer.tar.gz
 tar -xf ~/inst/installer.tar.gz -C ~/inst
 ~/inst/update.sh -i -c stable -p ~/node -d ~/node/data -n
 
 # don't comment this out; tests depend on the specific network setup
 cp template.json ~/node
+
+
+
+
