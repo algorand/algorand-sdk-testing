70c31bf379a12e1da5a99aca742080518c9efcf2
mjiang102628 <mjiang102628@gmail.com>
2019-08-01 14:02:44 -0400

    Update setup.sh

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -56,7 +56,8 @@ if $java
 then
     if [[ $TRAVIS_PULL_REQUEST != false ]]
     then
-        mvn versions:use-dep-version -Dincludes=com.github.algorand:java-algorand-sdk -DdepVersion="${TRAVIS_PULL_REQUEST_BRANCH}-SNAPSHOT" -DforceVersion=true
+        latestcommit="${TRAVIS_PULL_REQUEST_BRANCH////~}"
+        mvn versions:use-dep-version -Dincludes=com.github.algorand:java-algorand-sdk -DdepVersion="${latestcommit}-SNAPSHOT" -DforceVersion=true
     else
         mvn versions:use-dep-version -Dincludes=com.github.algorand:java-algorand-sdk -DdepVersion=$TRAVIS_COMMIT -DforceVersion=true
     fi
