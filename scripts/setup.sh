0177a4b50ade8d725df5fcc365cf6ee386e5b12f
mjiang102628 <mjiang102628@gmail.com>
2020-02-10 08:57:09 -0500

    Update setup.sh

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -24,7 +24,7 @@ esac
 go get github.com/DATA-DOG/godog/cmd/godog
 if $go
 then
-    go get -u github.com/algorand/go-algorand-sdk/...@develop
+    go get -v -u github.com/algorand/go-algorand-sdk/...
     go generate github.com/algorand/go-algorand-sdk/...
 fi
 
