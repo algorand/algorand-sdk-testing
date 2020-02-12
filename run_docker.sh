cb931e068464a60965001915afec719ba3b520de
Will Winder <wwinder.unh@gmail.com>
2020-02-12 15:13:05 -0500

    Switch back run_docker.sh

--- run_docker.sh
+++ run_docker.sh
@@ -7,16 +7,17 @@ pushd $rootdir
 docker build -t sdk-testing -f ./scripts/docker/Dockerfile $rootdir
 
 # Example running local java sdk code in the container
-docker run -it \
-      -v $(pwd):/opt/sdk-testing \
-      -v /home/will/algorand/java-algorand-sdk:/opt/mounted_java_sdk \
-      sdk-testing:latest \
-      /bin/bash -c "\
-            GO111MODULE=off && \
-            ./scripts/docker/setup.py --algod-config scripts/config_future --java-dir /opt/mounted_java_sdk && \
-            ./scripts/docker/test.py --algod-config scripts/config_future --sdk-testing /opt/sdk-testing/ --network-dir /opt/testnetwork"
+#docker run -it \
+#      -v $(pwd):/opt/sdk-testing \
+#      -v /home/will/algorand/java-algorand-sdk:/opt/mounted_java_sdk \
+#      sdk-testing:latest \
+#      /bin/bash -c "\
+#            GO111MODULE=off && \
+#            ./scripts/docker/setup.py --algod-config scripts/config_future --java-dir /opt/mounted_java_sdk && \
+#            ./scripts/docker/test.py --algod-config scripts/config_future --sdk-testing /opt/sdk-testing/ --network-dir /opt/testnetwork"
+
 #            ./scripts/docker/test.sh --java"
 
 #docker run -it -v $(pwd):/opt/sdk-testing sdk-testing:latest /bin/bash -c "GO111MODULE=off && ./scripts/docker/setup.py --algod-config scripts/config_future && ./scripts/test.sh"
 
-#docker run -it -v $(pwd):/opt/sdk-testing sdk-testing:latest
+docker run -it -v $(pwd):/opt/sdk-testing sdk-testing:latest
