a674ea58af82061a280939bfe7bfd25486fca220
Evan Richard <evan.richard@algorand.com>
2020-03-16 20:23:49 -0400

    @develop not @evan/templateshotfix

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -24,7 +24,7 @@ esac
 
 if ! $go
 then
-    go get -u github.com/algorand/go-algorand-sdk/...@evan/templateshotfix
+    go get -u github.com/algorand/go-algorand-sdk/...@develop
     go generate github.com/algorand/go-algorand-sdk/...
 fi
 go get github.com/cucumber/godog/cmd/godog/...
@@ -83,6 +83,6 @@ source "$SCRIPTPATH/config_future"
 
 mkdir ~/inst
 # this is the link for linux; change this if on mac or windows
-curl -L https://algorand-releases.s3.amazonaws.com/channel/stable/install_stable_linux-amd64_2.0.4.tar.gz -o ~/inst/installer.tar.gz
+curl -L https://algorand-releases.s3.amazonaws.com/channel/stable/install_stable_darwin-amd64_2.0.4.tar.gz -o ~/inst/installer.tar.gz
 tar -xf ~/inst/installer.tar.gz -C ~/inst
 ~/inst/update.sh -i -c $CHANNEL -p $BIN_DIR -d $BIN_DIR/data -n
