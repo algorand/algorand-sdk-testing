35208f504b526ae316404f44286cbaf6065ce10b
Will Winder <wwinder.unh@gmail.com>
2020-05-21 10:22:13 -0400

    Make down.sh even more reliable.

--- scripts/down.sh
+++ scripts/down.sh
@@ -5,6 +5,7 @@ pushd $rootdir/.. > /dev/null
 docker-compose down
 docker-compose rm --force
 
-# In case a graceful shutdown fails, bring them down the hard way.
+# In case a graceful shutdown/remove fails, bring them down the hard way.
 #docker kill $(docker ps -f name="sdk-harness")
-docker ps -f name="sdk-harness" -q | xargs -r docker kill
+docker ps -a -f name="sdk-harness" -q | xargs -r docker kill
+docker ps -a -f name="sdk-harness" -q | xargs -r docker rm
