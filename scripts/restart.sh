3f535367ab8b3373bfd0f62ba5cf349457731a11
algonautshant <shant@algorand.com>
2020-05-26 13:32:39 -0400

    Update the reset.sh script.

--- scripts/restart.sh
+++ scripts/restart.sh
@@ -2,14 +2,19 @@
 
 # This script is useful for development to quickly restart the
 # environment for another round of tests.
-set -e
 
-rootdir=`dirname $0`
-pushd $rootdir/.. > /dev/null
+# This should run only after up.sh and before down.sh
 
-# Make sure it isn't running
-./scripts/down.sh
+# Use this script for the following situation(s):
 
-# Make sure the state is reset.
+# Some tests alter the state of the blockchain, and running the again
+# requires the initial blockchain state.
+# For example: when the test rekeys an account, running the test again
+# will fail to rekey using the initial key.
+# Running this script between test runs will reset the blockchain state.
+
+docker-compose down
 docker-compose rm --force
 docker-compose up -d
+
+
