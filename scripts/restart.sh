84b989e6d3eb04feb9ea533289142539e5325306
algonautshant <55754073+algonautshant@users.noreply.github.com>
2020-05-31 20:09:43 -0400

    Update the restart.sh script. (#82)

--- scripts/restart.sh
+++ scripts/restart.sh
@@ -7,9 +7,14 @@ set -e
 rootdir=`dirname $0`
 pushd $rootdir/.. > /dev/null
 
-# Make sure it isn't running
-./scripts/down.sh
+# Use this script for the following situation(s):
 
-# Make sure the state is reset.
+# Some tests alter the state of the blockchain, and running the again
+# requires the initial blockchain state.
+# For example: when the test rekeys an account, running the test again
+# will fail to rekey using the initial key.
+# Running this script between test runs will reset the blockchain state.
+
+docker-compose down
 docker-compose rm --force
 docker-compose up -d
