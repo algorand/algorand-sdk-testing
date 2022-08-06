f4734e2cade8ebf95002ee7681b5292ccd456923
Zeph Grunschlag <zeph@algorand.com>
2022-08-05 19:52:06 -0500

    yuck... this should have been a python script. sed is platform dependent

--- scripts/up.sh
+++ scripts/up.sh
@@ -14,15 +14,6 @@ ENV_FILE=".env"
 echo "up.sh is sourcing environment vars from-->$ENV_FILE"
 source "$ENV_FILE"
 
-# Verify there are no positional parameters with getopt/getopts
-shift "$((OPTIND-1))"
-if [[ "$1" != "" ]]; then
-  echo "No positional arguments should be provided, found '$@'"
-  echo
-  show_help
-  exit
-fi
-
 # Choose which dockerfile to use.
 TYPE=${TYPE_OVERRIDE:-$TYPE}
 if [[ $TYPE == "channel" ]] || [[ $TYPE == "source" ]]; then
@@ -49,15 +40,27 @@ else
   ALGOD_CHANNEL=""
 fi
 
-sed -i "" "s|#ALGOD_CHANNEL|$ALGOD_CHANNEL|g" "$SANDBOX_CFG"
-sed -i "" "s|#ALGOD_URL|$ALGOD_URL|g" "$SANDBOX_CFG"
-sed -i "" "s|#ALGOD_BRANCH|$ALGOD_BRANCH|g" "$SANDBOX_CFG"
-sed -i "" "s|#ALGOD_SHA|$ALGOD_SHA|g" "$SANDBOX_CFG"
-sed -i "" "s|#NETWORK_TEMPLATE|$NETWORK_TEMPLATE|g" "$SANDBOX_CFG"
-sed -i "" "s|#NETWORK_NUM_ROUNDS|$NETWORK_NUM_ROUNDS|g" "$SANDBOX_CFG"
-sed -i "" "s|#INDEXER_URL|$INDEXER_URL|g" "$SANDBOX_CFG"
-sed -i "" "s|#INDEXER_BRANCH|$INDEXER_BRANCH|g" "$SANDBOX_CFG"
-sed -i "" "s|#INDEXER_SHA|$INDEXER_SHA|g" "$SANDBOX_CFG"
+if [[ $OSTYPE == 'darwin'* ]]; then
+  SED_CMD='sed -i ""'
+else
+  SED_CMD='sed -i'
+fi
+
+echo "On this OSTYPE=$OSTYPE using sed variant $SED_CMD"
+
+echo "apply regex replacements on SANDBOX_CFG=$SANDBOX_CFG"
+$SED_CMD "s|#ALGOD_CHANNEL|$ALGOD_CHANNEL|g" "$SANDBOX_CFG"
+$SED_CMD "s|#ALGOD_URL|$ALGOD_URL|g" "$SANDBOX_CFG"
+$SED_CMD "s|#ALGOD_BRANCH|$ALGOD_BRANCH|g" "$SANDBOX_CFG"
+$SED_CMD "s|#ALGOD_SHA|$ALGOD_SHA|g" "$SANDBOX_CFG"
+$SED_CMD "s|#NETWORK_TEMPLATE|$NETWORK_TEMPLATE|g" "$SANDBOX_CFG"
+$SED_CMD "s|#NETWORK_NUM_ROUNDS|$NETWORK_NUM_ROUNDS|g" "$SANDBOX_CFG"
+$SED_CMD "s|#INDEXER_URL|$INDEXER_URL|g" "$SANDBOX_CFG"
+$SED_CMD "s|#INDEXER_BRANCH|$INDEXER_BRANCH|g" "$SANDBOX_CFG"
+$SED_CMD "s|#INDEXER_SHA|$INDEXER_SHA|g" "$SANDBOX_CFG"
+
+echo "Resulting $SANDBOX_CFG:"
+cat "$SANDBOX_CFG"
 
 rootdir=$(dirname "$0")
 pushd "$rootdir"/.. > /dev/null || exit
