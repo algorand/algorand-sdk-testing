65710e90f653ad5c87210c82af1bc2c5ff7b877f
Zeph Grunschlag <zeph@algorand.com>
2022-08-07 20:37:45 -0500

    experiiment

--- scripts/down.sh
+++ scripts/down.sh
@@ -7,8 +7,8 @@ echo "down.sh is sourcing environment vars from-->$ENV_FILE"
 source "$ENV_FILE"
 echo "down.sh will be looking to clean up inside-->$SANDBOX_DIR"
 
-rootdir=$(dirname "$0")
-pushd "$rootdir"/.. > /dev/null || exit
+# rootdir=$(dirname "$0")
+# pushd "$rootdir"/.. > /dev/null || exit
 
 if [ -d "$SANDBOX_DIR" ]; then
   pushd "$SANDBOX_DIR"
