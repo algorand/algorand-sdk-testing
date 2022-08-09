ae4a25028d3c16b60734af31db09e2c68592ced0
Zeph Grunschlag <zeph@algorand.com>
2022-08-09 15:56:31 -0500

    verbose sandbox + clean up up.sh echos

--- scripts/up.sh
+++ scripts/up.sh
@@ -11,7 +11,7 @@ TYPE_OVERRIDE=""
 ENV_FILE=".env"
 
 # Load environment
-echo "up.sh is sourcing environment vars from-->$ENV_FILE"
+echo "up.sh: sourcing environment vars from-->$ENV_FILE"
 source "$ENV_FILE"
 
 # Choose which dockerfile to use.
@@ -19,21 +19,16 @@ TYPE=${TYPE_OVERRIDE:-$TYPE}
 if [[ $TYPE == "channel" ]] || [[ $TYPE == "source" ]]; then
   export TYPE="$TYPE"
 else
-  echo "Unknown environment: $TYPE"
+  echo "up.sh: Unknown environment: $TYPE"
   exit 1
 fi
 
-echo "Before bootrapping, try cleaning up first..."
+echo "up.sh: Before bootrapping, try cleaning up first..."
 
-
-pwd
-ls -a
-echo "^^^^CWD STATUS FROM up.sh^^^^"
-
-# Make sure it isn't running and clean up any docker detritous
+# Make sure test-sdk sandbox isn't running and clean up any docker detritous
 ./scripts/down.sh -f "$ENV_FILE"
 rm -rf "$SANDBOX_DIR"
-echo "up.sh. seconds it took to get to end of $SANDBOX_DIR cleanup: " + $(($(date "+%s") - $START))
+echo "up.sh: seconds it took to get to end of $SANDBOX_DIR cleanup: " + $(($(date "+%s") - $START))
 
 SANDBOX_CFG="_config.harness"
 cp config.harness "$SANDBOX_CFG"
@@ -51,9 +46,8 @@ else
   SED_CMD='sed -i '
 fi
 
-echo "On this OSTYPE=$OSTYPE using sed variant $SED_CMD"
-
-echo "apply regex replacements on SANDBOX_CFG=$SANDBOX_CFG"
+echo "up.sh: On this OSTYPE=$OSTYPE using sed variant $SED_CMD"
+echo "up.sh: apply regex replacements on SANDBOX_CFG=$SANDBOX_CFG"
 $SED_CMD "s|#ALGOD_CHANNEL|$ALGOD_CHANNEL|g" "$SANDBOX_CFG"
 $SED_CMD "s|#ALGOD_URL|$ALGOD_URL|g" "$SANDBOX_CFG"
 $SED_CMD "s|#ALGOD_BRANCH|$ALGOD_BRANCH|g" "$SANDBOX_CFG"
@@ -66,7 +60,7 @@ $SED_CMD "s|#INDEXER_URL|$INDEXER_URL|g" "$SANDBOX_CFG"
 $SED_CMD "s|#INDEXER_BRANCH|$INDEXER_BRANCH|g" "$SANDBOX_CFG"
 $SED_CMD "s|#INDEXER_SHA|$INDEXER_SHA|g" "$SANDBOX_CFG"
 
-echo "Resulting $SANDBOX_CFG:"
+echo "up.sh: resulting $SANDBOX_CFG:"
 cat "$SANDBOX_CFG"
 
 rootdir=$(dirname "$0")
@@ -74,12 +68,12 @@ pushd "$rootdir"/.. > /dev/null || exit
 
 git clone --branch "$SANDBOX_BRANCH" --single-branch "$SANDBOX_URL" "$SANDBOX_DIR"
 
-echo "up.sh. seconds it took to get to end of cloning sandbox into $SANDBOX_DIR: " + $(($(date "+%s") - $START))
-
-echo "Bringing up network with '$TYPE' configuration."
+echo "up.sh: seconds it took to get to end of cloning sandbox into $SANDBOX_DIR: " + $(($(date "+%s") - $START))
+echo ""
+echo "up.sh: bringing up network with '$TYPE' configuration."
 cp .env "$SANDBOX_DIR"/.
 mv "$SANDBOX_CFG" "$SANDBOX_DIR"/config.harness
 pushd "$SANDBOX_DIR"
 
-./sandbox up harness
-echo "up.sh. seconds it took to finish with harness ($SANDBOX_DIR) up and running: " + $(($(date "+%s") - $START))
+./sandbox up -v harness
+echo "up.sh: seconds it took to finish with harness ($SANDBOX_DIR) up and running: " + $(($(date "+%s") - $START))
